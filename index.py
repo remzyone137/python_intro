82f5a00c93c4761f72abfc0a74e4c192bab151d3
remzyone137 <remo36@protonmail.com>
2021-12-13 11:39:21 +0100

    initial commit

--- index.py
+++ index.py
@@ -1,20 +1,41 @@
+# Variables
 name="henry" # declare string type variable with value "henry"
 age=4 # type integer/number
 loggedIn=False # type boolean (True OR False)
-print("Welcome " + name + "!")
+# print("Welcome " + name + "!")
 users=["Remo", "Tommy", "Marie"] # decalre users variable of type array/list
-print (users) # prints the whole array
-print(users[0]) # index is based 0
-print(users[:1])
-print(users[1:]) # array slicing
+# print (users) # prints the whole array
+# print(users[0]) # index is based 0
+# print(users[:1])
+# print(users[1:]) # array slicing
 session = { # python object/dict, complex data structure
     "id": 231231,
     "name": "host",
     "createdAt": 170545353
 }
-print(session)
-print(session["name"]) # print just one key/entity of object/dict
+# print(session)
+# print(session["name"]) # print just one key/entity of object/dict
 
 # TODO
-# 1. conditionals
 # 2. for loops and interation
+
+# 1. conditionals
+
+age=14
+
+if age > 17:
+    print("Welcome")
+else:
+    print("Too yong")
+
+
+users=[14,33,52,78]
+for user in users:
+    print(user)
+    if user > 17:
+        print("Welcome")
+    else:
+        print("Too yong")
+
+
+print(age)
