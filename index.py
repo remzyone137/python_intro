0e21219f127a9a24f47bdc7ecf771af345c0c360
remzyone137 <remo36@protonmail.com>
2021-12-13 11:06:16 +0100

    initial commit

---
+++ index.py
@@ -0,0 +1,20 @@
+name="henry" # declare string type variable with value "henry"
+age=4 # type integer/number
+loggedIn=False # type boolean (True OR False)
+print("Welcome " + name + "!")
+users=["Remo", "Tommy", "Marie"] # decalre users variable of type array/list
+print (users) # prints the whole array
+print(users[0]) # index is based 0
+print(users[:1])
+print(users[1:]) # array slicing
+session = { # python object/dict, complex data structure
+    "id": 231231,
+    "name": "host",
+    "createdAt": 170545353
+}
+print(session)
+print(session["name"]) # print just one key/entity of object/dict
+
+# TODO
+# 1. conditionals
+# 2. for loops and interation
