4d4f2ac07e164db526a8f5433665dc25832645b4
remzyone137 <remo36@protonmail.com>
2022-01-07 12:07:17 +0100

    subsensitiv_information

--- weather.py
+++ weather.py
@@ -1,7 +1,11 @@
 import requests
-   
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+print (os.environ['BOTKEY'])   
 # Making a GET request
-r = requests.get('https://api.openweathermap.org/data/2.5/weather?q=Barcelona&appid=6b0c557e8fdcf766b82c030af6dc1b10')
+r = requests.get('https://api.openweathermap.org/data/2.5/weather?q=Barcelona&appid='+os.environ['WEATHERKEY'])
   
 # check status code for response received
 # success code - 200
@@ -12,4 +16,4 @@ conditions = data['weather'][0]['main']
 print(data['weather'][0]['main'])
 # 1489050951
 # curl -s -X POST https://api.telegram.org/bot5077725422:AAHMHH5S9_dzJt0UuSjMSCuwJ-BlTkonxFY/sendMessage -d chat_id=1489050951 -d text="Remo is the coolest1"
-r = requests.post('https://api.telegram.org/bot5077725422:AAHMHH5S9_dzJt0UuSjMSCuwJ-BlTkonxFY/sendMessage', data={'chat_id': 1489050951, 'text': 'Remo is the coolest1. And the weateher in barcelona is ' + conditions})
+r = requests.post('https://api.telegram.org/bot'+os.environ['BOTKEY']+'/sendMessage', data={'chat_id': 1489050951, 'text': 'Remo is the coolest1. And the weateher in barcelona is ' + conditions})
