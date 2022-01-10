6462da35f6a5084b7dbdf7a85cc2b216c39f04d4
remzyone137 <remo36@protonmail.com>
2022-01-10 10:49:33 +0100

    clean weather version+error handling

--- weather.py
+++ weather.py
@@ -1,19 +1,17 @@
-import requests
+import requests # perform http requests like GET or POST
 import os
 from dotenv import load_dotenv
-
 load_dotenv()
-print (os.environ['BOTKEY'])   
-# Making a GET request
-r = requests.get('https://api.openweathermap.org/data/2.5/weather?q=Barcelona&appid='+os.environ['WEATHERKEY'])
-  
-# check status code for response received
-# success code - 200
 
-# print content of request
+if "WEATHERKEY" in os.environ:
+    r = requests.get('https://api.openweathermap.org/data/2.5/weather?q=Barcelona&appid='+os.environ['WEATHERKEY'])
+else:
+    print("WEATHERKEY is not defined in .env")
+
 data = r.json()
 conditions = data['weather'][0]['main']
 print(data['weather'][0]['main'])
-# 1489050951
-# curl -s -X POST https://api.telegram.org/bot5077725422:AAHMHH5S9_dzJt0UuSjMSCuwJ-BlTkonxFY/sendMessage -d chat_id=1489050951 -d text="Remo is the coolest1"
-r = requests.post('https://api.telegram.org/bot'+os.environ['BOTKEY']+'/sendMessage', data={'chat_id': 1489050951, 'text': 'Remo is the coolest1. And the weateher in barcelona is ' + conditions})
+if "BOTKEY" in os.environ:
+    r = requests.post('https://api.telegram.org/bot'+os.environ['BOTKEY']+'/sendMessage', data={'chat_id': 1489050951, 'text': 'Remo is the coolest1. And the weateher in barcelona is ' + conditions})
+else:
+    print("BOTKEY is not defined in .env")
